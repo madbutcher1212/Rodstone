f2848e441b6d6c5cb68cf549ead71016f60a309a
madbutcher1212 <99019403+madbutcher1212@users.noreply.github.com>
2026-03-03 00:50:08 +0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 import os
+import sys  
 
 # Импорт blueprint'ов
 from blueprints.auth import auth_bp
