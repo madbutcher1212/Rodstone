57fd545b0eeb89cabc3c653f6d0e7a57a33be165
madbutcher1212 <99019403+madbutcher1212@users.noreply.github.com>
2026-03-03 00:36:25 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -4,6 +4,6 @@ import os
 # Добавляем папку backend в путь поиска модулей
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'backend'))
 
-from backend.app import create_app
+from backend.app import create_app  # или просто from app import create_app, зависит от структуры
 
 app = create_app()
