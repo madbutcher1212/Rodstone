f21f70ad3903f89fab1cb88b0bc4aae524548f36
madbutcher1212 <99019403+madbutcher1212@users.noreply.github.com>
2026-03-03 00:16:02 +0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -67,4 +67,4 @@ def create_app():
 app = create_app()
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
